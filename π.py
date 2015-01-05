91ce6f6484e11acdb582fad3afeea026200ede9f
Dave Andreoli <dave@gurumeditation.it>
2015-01-05 22:05:32 +0100

    more interface work

--- π.py
+++ π.py
@@ -1,33 +1,61 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from efl.evas import EVAS_HINT_EXPAND, EVAS_HINT_FILL, EXPAND_BOTH, FILL_BOTH
+from efl import evas
+from efl.evas import EVAS_TEXTGRID_PALETTE_STANDARD, \
+    EVAS_HINT_EXPAND, EXPAND_BOTH, EXPAND_HORIZ, EXPAND_VERT, \
+    EVAS_HINT_FILL, FILL_BOTH, FILL_HORIZ, FILL_VERT
+
 from efl import elementary
-from efl.elementary.window import StandardWindow, DialogWindow
-from efl.elementary.box import Box
-from efl.elementary.button import Button
+from efl.elementary.window import StandardWindow
 from efl.elementary.label import Label
-from efl.elementary.entry import Entry
+
 
 
 class PiWin(StandardWindow):
     def __init__(self, app):
 
-        StandardWindow.__init__(self, "pigreco", "π", resize=False, 
-                                autodel=True, size=(600, 600))
+        StandardWindow.__init__(self, "pigreco", "π", autodel=True)
         self.callback_delete_request_add(lambda o: elementary.exit())
 
-        box = Box(self, size=(600,600))# size_hint_weight=EXPAND_BOTH, size_hint_align=FILL_BOTH)
-        self.resize_object_add(box)
-        box.show()
-        
-        lb = Label(box, text="<title>- π -</title>", scale=10,
-                   size_hint_weight=EXPAND_BOTH, size_hint_align=FILL_BOTH)
-        box.pack_end(lb)
+        lb = Label(self, text="π", scale=20)
+        self.resize_object_add(lb)
         lb.show()
 
+        tg = TG(self.evas)
+        self.resize_object_add(tg)
+        tg.show()
+
+        self.tg = tg
         self.app = app
         self.show()
+
+
+class TG(evas.Textgrid):
+    width = 40
+    height = 15
+    def __init__(self, canvas):
+
+        evas.Textgrid.__init__(self, canvas,
+                               size=(self.width, self.height),
+                               font=("Courier", 24))
+
+        self.palette_set(EVAS_TEXTGRID_PALETTE_STANDARD, 0, 0, 0, 0, 100)
+        self.palette_set(EVAS_TEXTGRID_PALETTE_STANDARD, 1, 0, 255, 255, 255)
+
+        pxw, pxh = self.cell_size
+        self.size_hint_min=(pxw * self.width, pxh * self.height)
+
+        self.fill()
+
+    def fill(self):
+        for row_count in range(self.height):
+            row = self.cellrow_get(row_count)
+            for cell in row:
+                cell.codepoint = 'π'
+                cell.fg = 1
+                cell.bg = 0
+            self.cellrow_set(row_count, row)
 
 
 class Pigreco(object):
