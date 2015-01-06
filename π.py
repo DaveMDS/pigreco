5ba9883af0c6e3767e8faf3ad26f64687efa32ec
Dave Andreoli <dave@gurumeditation.it>
2015-01-06 03:55:00 +0100

    Render the digits in the textblock

--- π.py
+++ π.py
@@ -3,23 +3,23 @@
 
 
 ##############################################################################
-# algoritmo "streaming" di Gibbons , Rabinowiz, Wagon,...
+# Unbounded Spigot Algorithms for the Digits of Pi
 # http://web.comlab.ox.ac.uk/oucl/work/jeremy.gibbons/publications/spigot.pdf
-# S. Rabinowitz and S. Wagon, A spigot algorithm for the digits of pi, Amer. Math. Monthly 102 (1995) 195–203.
+# S. Rabinowitz and S. Wagon
 
 # http://davidbau.com/archives/2010/03/14/python_pipy_spigot.html
-def pi_decimal_digits():
+def pi_decimal_generator():
   q, r, t, j = 1, 180, 60, 2
   while True:
     u, y = 3*(3*j+1)*(3*j+2), (q*(27*j-12)+5*r)//(5*t)
-    yield y
+    yield str(y)
     q, r, t, j = 10*q*j*(2*j-1), 10*u*(q*(5*j-2)+r-y*t), t*u, j+1
 
 ## Usage:
 # count = 0
-# digits = pi_decimal_digits()
+# digits = pi_decimal_generator()
 # while 1:
-#   line = ''.join([str(digits.next()) for j in xrange(50)])
+#   line = ''.join([digits.next() for j in xrange(50)])
 #   print '%6d: %s' % (count, line)
 #   count += 50
 
@@ -38,6 +38,8 @@ from efl.elementary.window import StandardWindow
 from efl.elementary.label import Label
 from efl.elementary.entry import Entry
 
+COLS = 40
+ROWS = 15
 
 class PigrecoThread(Process):
     def __init__(self, queue):
@@ -45,7 +47,7 @@ class PigrecoThread(Process):
 
         self._queue = queue
         self._stop = Event()
-        self._digit_generator = pi_decimal_digits()
+        self._generator = pi_decimal_generator()
 
     def stop(self):
         self._stop.set()
@@ -53,10 +55,11 @@ class PigrecoThread(Process):
     def run(self):
         count = 0
 
+        self._generator.next()
         while 1:
-            line = ''.join([str(self._digit_generator.next()) for j in range(50)])
+            line = ''.join([self._generator.next() for j in range(COLS)])
             self._queue.put((count, line))
-            count += 50
+            count += COLS
 
             if self._stop.is_set():
                 break
@@ -64,15 +67,13 @@ class PigrecoThread(Process):
         print("DONE")
 
 
-
 class PiWin(StandardWindow):
     def __init__(self, app):
 
-        StandardWindow.__init__(self, "pigreco", "π", autodel=True, size=(500,500))
+        StandardWindow.__init__(self, "pigreco", "π", autodel=True)
         self.callback_delete_request_add(lambda o: app.quit())
 
-        lb = Label(self, text="π", scale=20, size_hint_weight=EXPAND_BOTH,
-                   size_hint_align=FILL_BOTH)
+        lb = Label(self, text="π", scale=15)
         self.resize_object_add(lb)
         lb.show()
 
@@ -80,63 +81,79 @@ class PiWin(StandardWindow):
         self.resize_object_add(tg)
         tg.show()
 
-
         self.tg = tg
         self.app = app
         self.show()
 
 
 class TG(evas.Textgrid):
-    width = 40
-    height = 15
     def __init__(self, parent):
-
-        self.lines = []
         evas.Textgrid.__init__(self, parent.evas,
-                               size=(self.width, self.height),
-                               font=("Courier", 24))
+                               size=(COLS, ROWS),
+                               font=("Monospace", 15))
 
         self.palette_set(EVAS_TEXTGRID_PALETTE_STANDARD, 0, 0, 0, 0, 100)
-        self.palette_set(EVAS_TEXTGRID_PALETTE_STANDARD, 1, 0, 255, 255, 255)
+        self.palette_set(EVAS_TEXTGRID_PALETTE_STANDARD, 1, 150, 150, 150, 255)
 
         pxw, pxh = self.cell_size
-        self.size_hint_min=(pxw * self.width, pxh * self.height)
+        self.size_hint_min = (pxw * COLS, pxh * ROWS)
 
-        self.fill()
-
-    def fill(self):
-        for row_count in range(self.height):
+        for row_count in range(ROWS):
             row = self.cellrow_get(row_count)
             for cell in row:
-                cell.codepoint = 'π'
+                cell.codepoint = ' '
                 cell.fg = 1
                 cell.bg = 0
             self.cellrow_set(row_count, row)
 
+        self.lines = []
+
+    def redraw(self, beg_line=None):
+        if beg_line is None:
+            beg_line = max(0, len(self.lines) - ROWS)
+
+        for row_num in range(ROWS):
+            if row_num >= len(self.lines):
+                break
+            row = self.cellrow_get(row_num)
+            for i, cell in enumerate(row):
+                cell.codepoint = self.lines[beg_line + row_num][i]
+            self.cellrow_set(row_num, row)
+
+        self.update_add(0, 0, COLS, ROWS)
+
+    def line_append(self, line):
+        self.lines.append(line)
+        self.redraw()
+
 
 class Pigreco(object):
     def __init__(self):
-
         self.win = PiWin(self)
         self.queue = Queue()
         self.thread = PigrecoThread(self.queue)
         self.thread.start()
 
-        ecore.Timer(0.1, self.timer_cb)
+        self.win.tg.lines.append('3.' + ' ' * (COLS-2))
 
-    def quit(self):
-        self.thread.stop()
-        self.thread.join()
-        elementary.exit()
-        
-    def timer_cb(self):
+        ecore.Timer(0.1, self._timer_cb)
+
+    def _timer_cb(self):
         if self.queue.empty():
             return ecore.ECORE_CALLBACK_RENEW
 
         count, line = self.queue.get()
         print(self.queue.qsize(), count, line)
 
+        self.win.tg.line_append(line)
+        self.win.title = 'π  (calculated decimals: %d)' % count
+
         return ecore.ECORE_CALLBACK_RENEW
+
+    def quit(self):
+        self.thread.stop()
+        self.thread.join()
+        elementary.exit()
 
 
 if __name__ == "__main__":
